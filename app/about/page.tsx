4e559e43a44e7413c32065772ce18e799c10b4e4
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-11-26 00:29:47 +0100

    Adjust navbar and footer

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,19 @@
 import { BlogPosts } from 'app/components/posts'
 
+export const metadata = {
+  title: 'About me',
+  description: 'Bio.',
+}
+
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        About me
       </h1>
       <p className="mb-4">
-        {`I like to build stuff in dotnet.`}
+        {`My name is Konrad Sałkowski.`}
       </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
     </section>
   )
 }
