d157228dd1eee6efa1e01cc0ef9270f3f4fe01f0
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-12-22 02:00:22 +0100

    Add small photo of myself in navbar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 const navItems = {
   '/': {
@@ -13,6 +14,13 @@ const navItems = {
 }
 
 export function Navbar() {
+  const imageData = {
+    src: '/images/me.jpg',
+    title: 'me',
+    width: 60,
+    height: 60,
+  };
+
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
@@ -20,7 +28,7 @@ export function Navbar() {
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-row w-full items-center space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
@@ -32,6 +40,15 @@ export function Navbar() {
                 </Link>
               )
             })}
+            <div className="ml-auto transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2">
+              <Image
+                src={imageData.src}
+                alt={imageData.title}
+                width={imageData.width}
+                height={imageData.height}
+                className="rounded-full"
+              />
+            </div>
           </div>
         </nav>
       </div>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     // Update this with your GitHub Pages URL
     output: 'export',
+    images: {
+      unoptimized: true,
+    }
     // basePath: '/your-repo-name',
     // assetPrefix: '/your-repo-name/',
     // trailingSlash: true, // Add trailing slash for GitHub Pages to handle routing correctly
