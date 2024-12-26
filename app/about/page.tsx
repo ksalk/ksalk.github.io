9c036e89663656250d2b5593806992cf329bb1c0
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-12-26 23:10:16 +0100

    Remove selfie from about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,29 +6,14 @@ export const metadata = {
 }
 
 export default function Page() {
-  const imageData = {
-    src: '/images/me.jpg',
-    title: 'me',
-    width: 150,
-    height: 150,
-  };
 
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         about me
       </h1>
-      <p>
-        <Image
-          src={imageData.src}
-          alt={imageData.title}
-          width={imageData.width}
-          height={imageData.height}
-          className="rounded-full m-3"
-        />
-      </p>
       <p className="mb-4">
-        {`I'm Konrad Sałkowski and I live in Warsaw PL.`}
+        {`I'm Konrad Sałkowski, and I live in Warsaw PL.`}
       </p>
       <p className="mb-4">
         {`I'm a .NET software engineer that dabbles with coding since 2010. I specialize in .NET tech stack, building APIs and backend systems, but I'm always open to learn and make use of any technology that will help me in my projects.
