5f7a0c6d532abccfb9f41c5b6ebd06775c89bb77
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-12-29 16:38:45 +0100

    Update bio

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'About me',
@@ -13,11 +14,20 @@ export default function Page() {
         about me
       </h1>
       <p className="mb-4">
-        {`I'm Konrad Sałkowski, and I live in Warsaw PL.`}
+        {`I'm Konrad Sałkowski, a .NET developer and I live in Warsaw PL.`}
       </p>
       <p className="mb-4">
-        {`I'm a .NET software engineer that dabbles with coding since 2010. I specialize in .NET tech stack, building APIs and backend systems, but I'm always open to learn and make use of any technology that will help me in my projects.
+        {`I'm a software engineer that dabbles with coding since 2010. I specialize in .NET tech stack, building APIs and backend systems, but I'm always open to learn and make use of any technology that will help me in my projects.
         My main interests lie in software architecture, optimization and algorithms.`}
+      </p>
+      <p className="mb-4">
+        {`If you want more info about my professional career, check out my `}
+        <Link
+          href="https://www.linkedin.com/in/konrad-salkowski/"
+          className='text-teal-400'>
+          LinkedIn page
+        </Link>
+        {`.`}
       </p>
     </section>
   )
