0f0bd859993d8751fb2c07ae5146e0233319bebd
Konrad Sałkowski <konrad.salkowski@gmail.com>
2025-01-09 00:28:05 +0100

    Update bio page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
         about me
       </h1>
       <p className="mb-4">
-        {`I'm Konrad Sałkowski, a .NET developer living in Warsaw PL.`}
+        {`I'm Konrad Sałkowski, a .NET developer located in Warsaw PL.`}
       </p>
       <p className="mb-4">
         {`I'm a software engineer that dabbles with coding since 2010. I specialize in .NET tech stack, building APIs and backend systems, but I'm always open to learn and make use of any technology that will help me in my projects.
