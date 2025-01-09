890fbd04ccc6897557ae56a19520564278f44a53
Konrad Sałkowski <konrad.salkowski@gmail.com>
2025-01-09 14:37:16 +0100

    Increase footer margin

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,7 +18,7 @@ function ArrowIcon() {
 // TODO: add links with icons to LI / RSS / GH / X
 export default function Footer() {
   return (
-    <footer className="mb-16">
+    <footer className="mb-16 mt-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
