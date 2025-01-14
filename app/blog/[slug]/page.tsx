657c8eb39edd189dde0d690aa84aee6cf58d0d12
Konrad Sałkowski <konrad.salkowski@gmail.com>
2025-01-14 16:22:00 +0100

    Change post date format

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -85,7 +85,7 @@ export default function Blog({ params }) {
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          {formatDate(post.metadata.publishedAt)}
+          {post.metadata.publishedAt}
         </p>
       </div>
       <article className="prose">
