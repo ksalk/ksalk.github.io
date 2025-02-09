5afe1ddbf88b3ad062b5d3eaceae77e93f12c601
Konrad Sałkowski <konrad.salkowski@gmail.com>
2025-02-09 22:58:45 +0100

    Unlink post date

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -16,20 +16,21 @@ export function BlogPosts() {
           return 1
         })
         .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <div 
+              key={post.slug}
+              className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
                 {post.metadata.publishedAt || 'draft'}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
+              <Link
+                className="flex flex-col space-y-1 mb-4"
+                href={`/blog/${post.slug}`}
+              >
+                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                  {post.metadata.title}
+                </p>
+              </Link>
             </div>
-          </Link>
         ))}
     </div>
   )
