4cdb89ffd4f68471fc6182a4de28ed1833cc5a6b
Konrad Sałkowski <konrad.salkowski@gmail.com>
2025-04-13 20:15:00 +0200

    Add limit param to blog posts component

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,8 +1,12 @@
-import Link from 'next/link'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
+import Link from 'next/link';
+import { getBlogPosts } from 'app/blog/utils';
 
-export function BlogPosts() {
-  let allBlogs = getBlogPosts(false)
+interface BlogPostsProps {
+  limit?: number;
+}
+
+export function BlogPosts({ limit }: BlogPostsProps) {
+  let allBlogs = getBlogPosts(false);
 
   return (
     <div>
@@ -11,30 +15,31 @@ export function BlogPosts() {
           if (
             new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
           ) {
-            return -1
+            return -1;
           }
-          return 1
+          return 1;
         })
+        .slice(0, limit || allBlogs.length)
         .map((post) => (
-            <div 
-              key={post.slug}
-              className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <Link
-                className="flex flex-col space-y-1 mb-8 w-full !no-underline"
-                href={`/blog/${post.slug}`}
-              >
-                <p className="blog-list-title">
-                  {post.metadata.title}
-                </p>
-                <p className="blog-list-desc">
-                  {post.metadata.summary}
-                </p>
-                <p className="blog-list-date">
-                  {post.metadata.publishedAt}
-                </p>
-              </Link>
-            </div>
+          <div 
+            key={post.slug}
+            className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <Link
+              className="flex flex-col space-y-1 mb-8 w-full !no-underline"
+              href={`/blog/${post.slug}`}
+            >
+              <p className="blog-list-title">
+                {post.metadata.title}
+              </p>
+              <p className="blog-list-desc">
+                {post.metadata.summary}
+              </p>
+              <p className="blog-list-date">
+                {post.metadata.publishedAt}
+              </p>
+            </Link>
+          </div>
         ))}
     </div>
-  )
+  );
 }
